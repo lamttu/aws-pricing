dcb0f630b1c290f42d31c18a51f8280cbc051cad
Lam <lamtu1008@gmail.com>
2019-08-29 17:13:11 +1000

    Change sample.js to use Pricing API)

--- sample.js
+++ sample.js
@@ -1,36 +1,60 @@
 /*
  * Copyright 2013. Amazon Web Services, Inc. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-**/
+ **/
 
 // Load the SDK and UUID
-var AWS = require('aws-sdk');
-var uuid = require('node-uuid');
+var AWS = require("aws-sdk");
 
-// Create an S3 client
-var s3 = new AWS.S3();
+AWS.config.update({ region: "us-east-1" });
+var pricing = new AWS.Pricing();
 
-// Create a bucket and upload something into it
-var bucketName = 'node-sdk-sample-' + uuid.v4();
-var keyName = 'hello_world.txt';
+var params = {
+  FormatVersion: "aws_v1",
+  MaxResults: 1,
+  ServiceCode: "AmazonEC2",
+  Filters: [
+    {
+      Field: "ServiceCode",
+      Type: "TERM_MATCH",
+      Value: "AmazonEC2"
+    },
+    {
+      Field: "instanceType",
+      Type: "TERM_MATCH",
+      Value: "r5d.8xlarge"
+    },
+    {
+      Field: "location",
+      Type: "TERM_MATCH",
+      Value: "Asia Pacific (Sydney)"
+    },
+    {
+      Field: "offerTermCode",
+      Type: "TERM_MATCH",
+      Value: "JRTCKXETXF" //On-demand
+    }
+  ]
+};
 
-s3.createBucket({Bucket: bucketName}, function() {
-  var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-  s3.putObject(params, function(err, data) {
-    if (err)
-      console.log(err)
-    else
-      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-  });
+pricing.getProducts(params, function(err, data) {
+  if (err) console.log(err, err.stack);
+  // an error occurred
+  else {
+    console.log("got the price");
+    var priceList = data["PriceList"][0]["terms"]["OnDemand"];
+    var result = JSON.stringify(priceList);
+    console.log(result); // successful response
+  }
 });
